92076ba2473497ece1060d327bd7cd173fc58855
Marcus 'Loki' Jenz <marcus.jenz@googlemail.com>
2014-06-08 15:01:05 +0200

    added ourTools for Nameservice Methods

---
+++ src/ourTools.erl
@@ -0,0 +1,25 @@
+%%%-------------------------------------------------------------------
+%%% @author Loki
+%%% @copyright (C) 2014, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 08. Jun 2014 14:36
+%%%-------------------------------------------------------------------
+-module(ourTools).
+-author("Loki").
+
+%% API
+-export([registerWithNameService/2, lookupNamewithNameService/2, unbindOnNameService/2]).
+
+registerWithNameService(Name, Nameservice) ->
+  ok
+  .
+
+lookupNamewithNameService(Name, Nameservice) ->
+  ok
+.
+
+unbindOnNameService(Name, Nameservice) ->
+  ok
+.
